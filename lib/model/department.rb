3277e12f7f308872dee585f7827d9287df02561b
hari shankar <hari.shankar@urbanladder.com>
2015-06-10 15:34:31 +0530

    1st test case passed for department initialize

--- lib/model/department.rb
+++ lib/model/department.rb
@@ -1,4 +1,9 @@
 #JOB: Defines basic structure of any department
 class Department
-
+  attr_reader :funding
+  def initialize(parent_department_name, department_name, funding)
+    @parent_department_name = parent_department_name
+    @name = department_name
+    @funding = funding
+  end
 end
